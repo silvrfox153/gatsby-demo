54b6de8a2740e8685e6726abe6d771355d15e60b
silvrfox153 <matt.tierney153@gmail.com>
2022-06-12 19:49:15 -0400

    firt commit

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+const ContactPage = () => {
+  return (
+    <main>
+      <title>Contact</title>
+      <h1>Contact</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Send me a note. I'd love to hear from you.</p>
+    </main>
+  );
+};
+
+export default ContactPage;
