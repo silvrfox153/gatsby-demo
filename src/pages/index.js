cec4a713344f5408d675a5cec3e54fe58fecca31
silvrfox153 <matt.tierney153@gmail.com>
2022-06-12 15:39:30 -0400

    commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  );
+};
+
+export default IndexPage;
